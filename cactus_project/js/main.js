86c3f5fe6af9e6cf82db42eecd30b351d40465c7
efealp4 <efealp1664@hotmail.com>
2025-03-13 03:58:45 +0300

    Upload
    upload

--- cactus_project/js/main.js
+++ cactus_project/js/main.js
@@ -27,3 +27,43 @@ const linkAction= ()=> {
 }
 
 navLink.forEach(n=> n.addEventListener('click',linkAction)); 
+
+/* ====== ADD BLUR HEADER ====== */ 
+const blurHeader = () => {
+    const header= document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport. 
+    this.scrollY >= 50 ? header.classList.add('blur-header') :
+    header.classList.remove('blur-header')
+}
+window.addEventListener('scroll',blurHeader)
+
+/* ====== SHOW SCROLL UP ====== */ 
+const scrollUp= ()=> {
+    const scrollUp= document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll to the a tag with the scrollup class.
+    this.scrollY>= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp);
+
+/* ====== SCROLL SECTIONS ACTIVE LINK ====== */ 
+const sections= document.querySelectorAll('section[id]')
+
+const scrollActive= ()=> {
+    const scrollDown= window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight= current.offsetHeight, 
+            sectionTop= current.offsetTop -58, 
+            sectionId= current.getAttribute('id'),
+            sectionClass= document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+        
+        if(scrollDown> sectionTop && scrollDown<= sectionTop + sectionHeight){
+            sectionClass.classList.add('active-link')
+        }
+        else{
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll',scrollActive)
+
